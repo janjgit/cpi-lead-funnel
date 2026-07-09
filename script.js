30eb395c8fc80b2720a2927883dd475878aa7444
janjgit <janjelinek@ourvitalmission.com>
2026-07-09 14:48:37 +0200

    Use standard Lead tracking event

--- script.js
+++ script.js
@@ -246,7 +246,7 @@ function trackLeadConversion() {
   window.__cpiLeadTracked = true;
 
   if (typeof window.fbq === "function") {
-    window.fbq("trackSingle", "2154388281488546", "Lead", {
+    window.fbq("track", "Lead", {
       content_name: "Kostenloser Enterprise SaaS-Prototyp",
       content_category: "Lead Funnel"
     });
